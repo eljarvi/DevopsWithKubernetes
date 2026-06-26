a5ef95ddf1570d6298ee08069611a9ae5c079ffe
Ellen Järvinen <jarvi.ellen@gmail.com>
2026-06-26 13:46:55 +0300

    Complete exercise 1.5

--- the_project/todo-app/README.md
+++ the_project/todo-app/README.md
@@ -6,6 +6,12 @@ docker build -t todo-app .
 
 docker run -e PORT="PORT" -p "HOSTPORT":"CONTAINER-PORT" todo-app
 
-## Deploy with Kubernetes
+## Deploy with Kubernetes and check that it works
 
 kubectl apply -f manifests/deployment.yaml
+
+kubectl get pods
+
+kubectl port-forward todo-app-dep-7648bc994d-p5fwx HOSTPORT:PORT
+
+
--- the_project/todo-app/index.js
+++ the_project/todo-app/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Todo app is running");
+    res.send(`
+        <html>
+            <head>
+                <title>Todo App</title>
+            </head>
+            <body>
+                <h1>Todo app is running </h1>
+            </body>
+        </html>
+    `);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started in port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
